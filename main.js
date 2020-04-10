60b3ff3963ae59d79a5a06811738e7e2b487f538
Sharod Bailey <Sharod.Bailey@codeimmersives.com>
2020-04-09 21:02:55 -0400

    almost there

--- main.js
+++ main.js
@@ -1,10 +1,48 @@
 class Vehicle {
+  // Setup object
+  constructor (name,iGotThisMany = 4,vRoom) {
+    //instance properties
+    this.name = name;
+    this.wheelCount = iGotThisMany;
+    this./*my current obj*/maxSpeed = vRoom; 
+    this.speed = 0;
+  }
+  accelerate(){
+    this.speed = this.maxSpeed / 3 + this.speed; //increasing speed and reassigning
+    
+    }
 }
+  
 
-class Car {
-}
+let myVehicle = new Vehicle();// calling my constructor func
+console.log(myVehicle)
 
-class Ferrari {
+class Car extends Vehicle {
+  constructor(name,maxSpeed){
+    super(name);
+      this.name = name;
+      this.maxSpeed = maxSpeed;
+    }
+    drive() {
+      return "Vroom. "
+    }
+  }
+
+class Ferrari extends Car {
+  constructor(name){
+  super(name = "Ferrari");
+    this.name = name;
+    this.maxSpeed = 180;
+
+  }
+  drive() {
+    return 'Vroom. I\'m a Ferrari!'
+  }
+
+  accelerate() {
+    this.speed = this.maxSpeed / 2 + this.speed; 
+    this.maxSpeed = 180;//eh??
+  }
 }
 
 
